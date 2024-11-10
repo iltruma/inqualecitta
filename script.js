9d539c46c5180eabe5bfd0ee51e3b3f96c842191
Cosimo Casini <casini.cosimo@gmail.com>
2024-11-10 15:45:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const trombaAudio = new Audio('suoni/tromba.mp3');
 const tarantellaAudio = new Audio('suoni/tarantella.mp3');
+const cities = ["Torino?", "Bolzano?", "Bressanone?", "Milano?", "Firenze?", "Genova?", "Trieste?", "Bergamo?", "Verona?", "Piacenza?", "Novara?"];
+let cityIndex = 0;
+
+document.getElementById("citta").innerText = cities[cityIndex];
 
 document.getElementById('play-tromba').addEventListener('click', () => {
     stopAudio();
     trombaAudio.play();
+    document.getElementById("citta").innerText = cities[cityIndex];
+    cityIndex = (cityIndex + 1) % cities.length;
 });
 
 document.getElementById('play-tarantella').addEventListener('click', () => {
     stopAudio();
     tarantellaAudio.play();
+    document.getElementById("citta").innerText = "E VAIIIIIIIII AMICO MIO";
 });
 
 document.getElementById('stop-audio').addEventListener('click', stopAudio);
@@ -18,16 +25,5 @@ function stopAudio() {
     tarantellaAudio.pause();
     trombaAudio.currentTime = 0;
     tarantellaAudio.currentTime = 0;
+    document.getElementById("citta").innerText = "";
 }
-
-const cities = ["Torino?", "Bolzano?", "Bressanone?", "Milano?", "Firenze?", "Genova?", "Trieste?", "Bergamo?", "Verona?", "Piacenza?", "Novara?"];
-let cityIndex = 0;
-
-document.getElementById("play-tromba").onclick = function () {
-    document.getElementById("citta").innerText = cities[cityIndex];
-    cityIndex = (cityIndex + 1) % cities.length;
-};
-
-document.getElementById("play-tarantella").onclick = function () {
-    document.getElementById("citta").innerText = "E VAIIIIIIIII AMICO MIO";
-};
